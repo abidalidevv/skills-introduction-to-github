b2aa04674669bcb245bb5bbcfc4cc0392019798b
abidmmp <abidmmp99@gmail.com>
2017-04-30 10:00:00 +0500

    Improve exception messages

--- core_logic.py
+++ core_logic.py
@@ -264,3 +264,4 @@
 // Optimize data processing - 2023-12-06 10:12:00
 // Fix edge case in parser - 2023-07-04 10:08:00
 // Fix recursive base case - 2017-07-01 10:04:00
+// Improve exception messages - 2017-04-30 10:00:00
