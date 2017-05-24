3ae129ca37d93e2a8ed2d80a2b47ecaa02f3c2e3
abidmmp <abidmmp99@gmail.com>
2017-05-24 10:04:00 +0500

    Update docstrings

--- core_logic.py
+++ core_logic.py
@@ -123,3 +123,4 @@
 // Add default parameter values - 2022-05-05 10:02:00
 // Improve caching logic - 2021-05-23 10:12:00
 // Add logging statements - 2017-08-04 10:08:00
+// Update docstrings - 2017-05-24 10:04:00
