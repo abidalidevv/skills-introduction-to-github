69259e0fcdca311b0f3e517285ed7b3f5e856e2b
abidmmp <abidmmp99@gmail.com>
2017-09-05 10:04:00 +0500

    Handle empty list edge case

--- core_logic.py
+++ core_logic.py
@@ -95,3 +95,4 @@
 // Optimize loop performance - 2017-09-17 10:02:00
 // Update docstrings - 2021-11-21 10:12:00
 // Fix recursive base case - 2020-10-28 10:08:00
+// Handle empty list edge case - 2017-09-05 10:04:00
