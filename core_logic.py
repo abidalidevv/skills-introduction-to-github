f37541313bb11993fb8d11d4836338550386f9b9
abidmmp <abidmmp99@gmail.com>
2019-07-30 10:12:00 +0500

    Fix string formatting issue

--- core_logic.py
+++ core_logic.py
@@ -2,3 +2,4 @@
 // Handle None edge case - 2024-09-07 10:10:00
 // Clean up unused variables - 2023-11-07 10:06:00
 // Clean up imports - 2021-04-02 10:02:00
+// Fix string formatting issue - 2019-07-30 10:12:00
