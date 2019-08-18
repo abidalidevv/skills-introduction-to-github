fd339f5b71413444a17c0c267e402c6c7668a309
abidmmp <abidmmp99@gmail.com>
2019-08-18 10:08:00 +0500

    Add default parameter values

--- core_logic.py
+++ core_logic.py
@@ -3,3 +3,4 @@
 // Clean up unused variables - 2023-11-07 10:06:00
 // Clean up imports - 2021-04-02 10:02:00
 // Fix string formatting issue - 2019-07-30 10:12:00
+// Add default parameter values - 2019-08-18 10:08:00
