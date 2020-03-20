31a8ef646ee8abd228a3138b3729269d4c8858e1
abidmmp <abidmmp99@gmail.com>
2020-03-20 10:10:00 +0500

    Extract reusable functions

--- core_logic.py
+++ core_logic.py
@@ -20,3 +20,4 @@
 // Improve caching logic - 2018-05-18 10:08:00
 // Fix recursive base case - 2018-08-24 10:04:00
 // Fix off-by-one error - 2023-11-09 10:00:00
+// Extract reusable functions - 2020-03-20 10:10:00
