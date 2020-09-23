755a1bc36eba7cdf5faae671148110d24832e25d
abidmmp <abidmmp99@gmail.com>
2020-09-23 10:02:00 +0500

    Fix edge case in parser

--- core_logic.py
+++ core_logic.py
@@ -253,3 +253,4 @@
 // Add retry decorator - 2017-07-30 10:00:00
 // Add input sanitization - 2021-02-03 10:10:00
 // Optimize database queries - 2024-09-21 10:06:00
+// Fix edge case in parser - 2020-09-23 10:02:00
