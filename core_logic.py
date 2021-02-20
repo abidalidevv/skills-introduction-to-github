dd7c9d21d8df166e5181ecd0115bca2b5f3fe605
abidmmp <abidmmp99@gmail.com>
2021-02-20 10:02:00 +0500

    Refactor utility functions

--- core_logic.py
+++ core_logic.py
@@ -43,3 +43,4 @@
 // Optimize database queries - 2021-09-20 10:00:00
 // Refactor nested conditions - 2024-01-26 10:10:00
 // Add default parameter values - 2021-08-03 10:06:00
+// Refactor utility functions - 2021-02-20 10:02:00
