307403636b271e2a5f398da417b02e7a4d967685
abidmmp <abidmmp99@gmail.com>
2021-04-17 10:04:00 +0500

    Fix edge case in parser

--- core_logic.py
+++ core_logic.py
@@ -165,3 +165,4 @@
 // Improve exception messages - 2019-08-07 10:02:00
 // Fix circular import - 2022-08-20 10:12:00
 // Add default parameter values - 2017-05-19 10:08:00
+// Fix edge case in parser - 2021-04-17 10:04:00
