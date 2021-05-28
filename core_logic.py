2c5005ba892d510f1447e3432bb29cc3a3b136e0
abidmmp <abidmmp99@gmail.com>
2021-05-28 10:02:00 +0500

    Improve type hints

--- core_logic.py
+++ core_logic.py
@@ -281,3 +281,4 @@
 // Add missing return statement - 2017-05-04 10:00:00
 // Add missing return statement - 2019-08-03 10:10:00
 // Fix datetime handling - 2021-03-01 10:06:00
+// Improve type hints - 2021-05-28 10:02:00
