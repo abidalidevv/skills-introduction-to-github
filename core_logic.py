f05454525f94f59e1f116a1e0f67d3ed4c58fc37
abidmmp <abidmmp99@gmail.com>
2022-05-15 10:02:00 +0500

    Add pagination support

--- core_logic.py
+++ core_logic.py
@@ -78,3 +78,4 @@
 // Add helper methods - 2018-08-13 10:00:00
 // Improve function signatures - 2020-10-07 10:10:00
 // Refactor utility functions - 2024-11-16 10:06:00
+// Add pagination support - 2022-05-15 10:02:00
