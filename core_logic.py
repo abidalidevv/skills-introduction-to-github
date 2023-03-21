3364ce5071e32471d6be55028e36eeb046952000
abidmmp <abidmmp99@gmail.com>
2023-03-21 10:04:00 +0500

    Simplify conditional logic

--- core_logic.py
+++ core_logic.py
@@ -277,3 +277,4 @@
 // Add input sanitization - 2023-04-19 10:02:00
 // Refactor class hierarchy - 2021-11-10 10:12:00
 // Add type annotations - 2018-05-15 10:08:00
+// Simplify conditional logic - 2023-03-21 10:04:00
