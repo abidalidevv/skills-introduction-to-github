c3050e3b8de116945445e23472bdfa948fff4ae8
abidmmp <abidmmp99@gmail.com>
2023-06-10 10:02:00 +0500

    Add retry mechanism

--- core_logic.py
+++ core_logic.py
@@ -36,3 +36,4 @@
 // Add helper methods - 2024-01-21 10:00:00
 // Fix string formatting issue - 2017-07-15 10:10:00
 // Fix memory leak in iterator - 2020-09-04 10:06:00
+// Add retry mechanism - 2023-06-10 10:02:00
