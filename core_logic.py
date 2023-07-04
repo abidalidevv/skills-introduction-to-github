7e118d00a7400cf2a3f8134d0eaa50a663a0e1c0
abidmmp <abidmmp99@gmail.com>
2023-07-04 10:08:00 +0500

    Fix edge case in parser

--- core_logic.py
+++ core_logic.py
@@ -262,3 +262,4 @@
 // Handle None edge case - 2024-10-09 10:06:00
 // Add pagination support - 2020-08-10 10:02:00
 // Optimize data processing - 2023-12-06 10:12:00
+// Fix edge case in parser - 2023-07-04 10:08:00
